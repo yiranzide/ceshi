6bd5bf71ccb31cd240aedecb4a72044490d43ea7
yiranzide <1028240348@qq.com>
2018-08-29 03:56:54 +0800

    修改 app

--- app.py
+++ app.py
@@ -13,9 +13,51 @@ from models.models import Comment
 from models.models import Question
 
 
+import redis
+
+
 app = Flask(__name__)
 db_path = 'forums.sqlite'
 manager = Manager(app)
+
+red = redis.Redis(host='localhost', port=6379, db=0)
+
+
+
+
+ns = Node.query.all()
+for n in ns:
+    red.lpush('l1', n.name)
+# d = red.lrange('l1', 0, 10)
+# for i in d:
+#     print(i.decode('utf-8'))
+
+u = User.query.count()
+q = Question.query.count()
+c = Comment.query.count()
+
+# print(u, q, c)
+red.set('user', u)
+red.set('question', q)
+red.set('comment', c)
+
+qs = Question.query.all()
+for q in qs:
+    red.set(q.id, q.click_count)
+
+cs = Comment.query.all()
+for c in cs:
+    u = User.query.get(c.user_id)
+    red.lpush('crl', {'node_id': c.node_id,
+               'username': u.username,
+               'created_time': c.created_time,
+               'content': c.content,
+               'question_id':c.question_id})
+
+# l = red.lrange('crl', 0, -1)
+# for i in l:
+#     print(i.decode('utf-8'))
+red.set('comment', 8)
 
 
 def register_routes(app):
